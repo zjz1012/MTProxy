511ca2d0c46b7fbbe4b9d54acef0b91804fd0892
zjz1012 <38127429+zjz1012@users.noreply.github.com>
2019-04-29 00:17:45 +0800

    Update MTProxy.sh

--- MTProxy.sh
+++ MTProxy.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: MTProxy
 #	Version: 1.0.5
-#	Author: zjz1012
+#	Author: shell-jc7
 #	Blog: https://doub.io/shell-jc7/
 #=================================================
 
